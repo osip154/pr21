335bed8ad22b5d5caef781e3c35f648bad488a06
osip154 <ilyaosip1208@gmail.com>
2026-06-19 20:52:23 +0700

    готово

--- app/src/androidTest/java/com/Osipov/Pr21_Osipov/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/Osipov/Pr21_Osipov/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.abdulaziz.pr21_mirzakamilov_pr23103;
+package com.Osipov.Pr21_Osipov;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.abdulaziz.pr21_mirzakamilov_pr23103", appContext.getPackageName());
+        assertEquals("com.Osipov.Pr21_Osipov", appContext.getPackageName());
     }
 }
