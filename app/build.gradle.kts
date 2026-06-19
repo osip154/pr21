f5b267aa61f32cbe6827ab961a48a6d46f37a443
osip154 <ilyaosip1208@gmail.com>
2026-06-19 20:56:04 +0700

    готово

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 android {
-    namespace = "com.abdulaziz.pr21_mirzakamilov_pr23103"
+    namespace = "com.Osipov.Pr21_Osipov"
     compileSdk {
         version = release(36) {
             minorApiLevel = 1
@@ -11,7 +11,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.abdulaziz.pr21_mirzakamilov_pr23103"
+        applicationId = "com.Osipov.Pr21_Osipov"
         minSdk = 31
         targetSdk = 36
         versionCode = 1
